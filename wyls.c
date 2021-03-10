7ed7493dec2d9bc08dc31a9bc067b489ce595818
Romello Wallace <47197674+RomelloWallace@users.noreply.github.com>
2021-03-09 20:30:12 -0700

    Update wyls.c

--- wyls.c
+++ wyls.c
@@ -1,6 +1,9 @@
-
-
-
+/*
+ Author: Romello Wallace
+ Date: 3/9/2021
+ Homework 5
+ This is a  program that replicates the n and h options of ls. 
+*/
 #include <stdio.h>
 
 #include <dirent.h>
@@ -21,19 +24,63 @@
 
 #include <grp.h>
 
+/* time format prints time last modifed and if the days are greater than one 180 it will print the year*/
+void timeFormat(struct stat buf) {
+
+  char time[50];
+  int days = 0;
+  days = strftime(time, 50, "%d", localtime( & buf.st_mtime));
+  if (days < 180) {
+
+    strftime(time, 50, "%B %d %H:%M", localtime( & buf.st_mtime));
+  } else {
+    strftime(time, 50, "%B %d %y %H:%M", localtime( & buf.st_mtime));
+  }
+
+  printf("%s", time);
+  printf(" ");
+
+};
+/*in an effort to clean up code a little, get IDs will prit the user and group uids*/
+void getIDs() {
+
+  printf("%d", getuid());
+  printf(" ");
+  printf("%d", getgid());
+  printf(" ");
+}
+/* RWX will print the file permissions in RWX format*/
+void RWX(struct stat buf) {
+
+  if (S_ISLNK(buf.st_mode)) {
+    printf((S_ISLNK(buf.st_mode)) ? "d" : "-");
+  } else {
+    printf((S_ISDIR(buf.st_mode)) ? "d" : "-");
+  }
+  printf((buf.st_mode & S_IRUSR) ? "r" : "-");
+  printf((buf.st_mode & S_IWUSR) ? "w" : "-");
+  printf((buf.st_mode & S_IXUSR) ? "x" : "-");
+  printf((buf.st_mode & S_IRGRP) ? "r" : "-");
+  printf((buf.st_mode & S_IWGRP) ? "w" : "-");
+  printf((buf.st_mode & S_IXGRP) ? "x" : "-");
+  printf((buf.st_mode & S_IROTH) ? "r" : "-");
+  printf((buf.st_mode & S_IWOTH) ? "w" : "-");
+  printf((buf.st_mode & S_IXOTH) ? "x" : "-");
+  printf(" ");
+}
 
 int main(int argc, char ** argv) {
+  /*lines 74 through87 are the various data types created and necessary for the program*/
   struct group * grp;
   struct passwd * user;
   char * getcwd(char * buf, size_t size);
-  int days;
+
   struct stat buf;
   DIR * d;
   struct dirent * dir;
   char * max = argv[argc - 1];
   long int bytesconvo;
 
-  char time[50];
   char * option1 = "-n";
   char * option2 = "-h";
   char * option3 = "-hn";
@@ -45,42 +92,61 @@ int main(int argc, char ** argv) {
   }
   for (int i = 1; i < argc; i++) {
     stat(max, & buf);
-
+    /*option 1 will format based of of n of ls method*/
     if (strchr(argv[i], '-') != NULL && strcmp(argv[i], option1) == 0) {
+      /*checks for directory, if it isa  directory it will open and start iterating through files*/
       if (S_ISDIR(buf.st_mode)) {
         d = opendir(max);
         if (d) {
           while ((dir = readdir(d))) {
             if (dir -> d_name[0] != '.') {
-              days = strftime(time, 50, "%d", localtime( & buf.st_mtime));
-              if (days < 180) {
-
-                strftime(time, 50, "%B %d %H:%M", localtime( & buf.st_mtime));
-              } else {
-                strftime(time, 50, "%B %d %y %H:%M", localtime( & buf.st_mtime));
-              }
-              if(S_ISLNK(buf.st_mode)){
-                printf((S_ISLNK(buf.st_mode)) ? "d" : "-");}
-                     else{
-              printf((S_ISDIR(buf.st_mode)) ? "d" : "-");}
-              printf((buf.st_mode & S_IRUSR) ? "r" : "-");
-              printf((buf.st_mode & S_IWUSR) ? "w" : "-");
-              printf((buf.st_mode & S_IXUSR) ? "x" : "-");
-              printf((buf.st_mode & S_IRGRP) ? "r" : "-");
-              printf((buf.st_mode & S_IWGRP) ? "w" : "-");
-              printf((buf.st_mode & S_IXGRP) ? "x" : "-");
-              printf((buf.st_mode & S_IROTH) ? "r" : "-");
-              printf((buf.st_mode & S_IWOTH) ? "w" : "-");
-              printf((buf.st_mode & S_IXOTH) ? "x" : "-");
-              printf(" ");
-              printf("%d", getuid());
-              printf(" ");
-              printf("%d", getgid());
-              printf(" ");
-
+              RWX(buf);
+              getIDs();
               printf("%ld", sizeof(dir -> d_name));
               printf(" ");
-              printf("%s", time);
+              timeFormat(buf);
+              printf(dir -> d_name);
+              printf("\n");
+            }
+          }
+          /*close directory once finished to avoid errors*/
+          closedir(d);
+        }
+      }
+      return 0;
+    }
+    /*option two will follow h of ls method*/
+    else if (strchr(argv[i], '-') != NULL && strcmp(argv[i], option2) == 0) {
+
+      grp = getgrgid(getgid());
+      user = getpwuid(getuid());
+      if (S_ISDIR(buf.st_mode)) {
+        d = opendir(max);
+        if (d) {
+          while ((dir = readdir(d))) {
+            if (dir -> d_name[0] != '.') {
+              RWX(buf);
+              printf("%s", user -> pw_name);
+              printf(" ");
+              printf("%s", grp -> gr_name);
+              printf(" ");
+              if (sizeof(dir -> d_name) > 1000 && sizeof(dir -> d_name) < 1000000) {
+                bytesconvo = sizeof(dir -> d_name) / 1000;
+                printf("%ld", bytesconvo);
+                printf("%s", "K");
+              } else if (sizeof(dir -> d_name) > 1000000 && sizeof(dir -> d_name) < 1000000000) {
+                bytesconvo = sizeof(dir -> d_name) / 1000000;
+                printf("%ld", bytesconvo);
+                printf("%s", "M");
+              } else if (sizeof(dir -> d_name) > 1000000000) {
+                bytesconvo = sizeof(dir -> d_name) / 1000000000;
+                printf("%ld", bytesconvo);
+                printf("%s", "G");
+              } else {
+                printf("%ld", sizeof(dir -> d_name));
+              }
+              printf(" ");
+              timeFormat(buf);
               printf(" ");
               printf(dir -> d_name);
               printf("\n");
@@ -90,37 +156,19 @@ int main(int argc, char ** argv) {
         }
       }
       return 0;
-    } else if (strchr(argv[i], '-') != NULL && strcmp(argv[i], option2) == 0) {
-
+    }
+    /*combines n and h option*/
+    else if (strchr(argv[i], '-') != NULL && strcmp(argv[i], option3) == 0) {
+      /*grp and user will print the names of the user and group who own the files*/
       grp = getgrgid(getgid());
       user = getpwuid(getuid());
+      /*check if argument is directory and iterate through if it is.*/
       if (S_ISDIR(buf.st_mode)) {
         d = opendir(max);
         if (d) {
           while ((dir = readdir(d))) {
             if (dir -> d_name[0] != '.') {
-
-              days = strftime(time, 50, "%d", localtime( & buf.st_mtime));
-              if (days < 180) {
-
-                strftime(time, 50, "%B %d %H:%M", localtime( & buf.st_mtime));
-              } else {
-                strftime(time, 50, "%B %d %y %H:%M", localtime( & buf.st_mtime));
-              }
-              if(S_ISLNK(buf.st_mode)){
-                printf((S_ISLNK(buf.st_mode)) ? "d" : "-");}
-                     else{
-              printf((S_ISDIR(buf.st_mode)) ? "d" : "-");}
-              printf((buf.st_mode & S_IRUSR) ? "r" : "-");
-              printf((buf.st_mode & S_IWUSR) ? "w" : "-");
-              printf((buf.st_mode & S_IXUSR) ? "x" : "-");
-              printf((buf.st_mode & S_IRGRP) ? "r" : "-");
-              printf((buf.st_mode & S_IWGRP) ? "w" : "-");
-              printf((buf.st_mode & S_IXGRP) ? "x" : "-");
-              printf((buf.st_mode & S_IROTH) ? "r" : "-");
-              printf((buf.st_mode & S_IWOTH) ? "w" : "-");
-              printf((buf.st_mode & S_IXOTH) ? "x" : "-");
-              printf(" ");
+              RWX(buf);
               printf("%s", user -> pw_name);
               printf(" ");
               printf("%s", grp -> gr_name);
@@ -143,19 +191,21 @@ int main(int argc, char ** argv) {
               }
 
               printf(" ");
-              printf("%s", time);
+              timeFormat(buf);
+              printf("%ld", sizeof(dir -> d_name));
+
               printf(" ");
               printf(dir -> d_name);
               printf("\n");
             }
           }
-          closedir(d);
         }
+        closedir(d);
       }
       return 0;
-    } else if (strchr(argv[i], '-') != NULL && strcmp(argv[i], option3) == 0) {
-
-     
+    }
+    /*combines n and h option of ls*/
+    else if (strchr(argv[i], '-') != NULL && strcmp(argv[i], option4) == 0) {
       grp = getgrgid(getgid());
       user = getpwuid(getuid());
       if (S_ISDIR(buf.st_mode)) {
@@ -163,33 +213,12 @@ int main(int argc, char ** argv) {
         if (d) {
           while ((dir = readdir(d))) {
             if (dir -> d_name[0] != '.') {
-
-              days = strftime(time, 50, "%d", localtime( & buf.st_mtime));
-              if (days < 180) {
-
-                strftime(time, 50, "%B %d %H:%M", localtime( & buf.st_mtime));
-              } else {
-                strftime(time, 50, "%B %d %y %H:%M", localtime( & buf.st_mtime));
-              }
-            if(S_ISLNK(buf.st_mode)){
-                printf((S_ISLNK(buf.st_mode)) ? "d" : "-");}
-                     else{
-              printf((S_ISDIR(buf.st_mode)) ? "d" : "-");}
-              printf((buf.st_mode & S_IRUSR) ? "r" : "-");
-              printf((buf.st_mode & S_IWUSR) ? "w" : "-");
-              printf((buf.st_mode & S_IXUSR) ? "x" : "-");
-              printf((buf.st_mode & S_IRGRP) ? "r" : "-");
-              printf((buf.st_mode & S_IWGRP) ? "w" : "-");
-              printf((buf.st_mode & S_IXGRP) ? "x" : "-");
-              printf((buf.st_mode & S_IROTH) ? "r" : "-");
-              printf((buf.st_mode & S_IWOTH) ? "w" : "-");
-              printf((buf.st_mode & S_IXOTH) ? "x" : "-");
-              printf(" ");
+              RWX(buf);
               printf("%s", user -> pw_name);
               printf(" ");
               printf("%s", grp -> gr_name);
               printf(" ");
-
+              /*byte combo is very messy but i couldnt figure out a proper function formatto clean it up*/
               if (sizeof(dir -> d_name) > 1000 && sizeof(dir -> d_name) < 1000000) {
                 bytesconvo = sizeof(dir -> d_name) / 1000;
                 printf("%ld", bytesconvo);
@@ -207,81 +236,21 @@ int main(int argc, char ** argv) {
               }
 
               printf(" ");
-              printf("%s", time);
+              timeFormat(buf);
+              printf("%ld", sizeof(dir -> d_name));
+
               printf(" ");
               printf(dir -> d_name);
               printf("\n");
             }
           }
-          closedir(d);
         }
+        closedir(d);
       }
       return 0;
-    } else if (strchr(argv[i], '-') != NULL && strcmp(argv[i], option4) == 0) {
-
-      grp = getgrgid(getgid());
-      user = getpwuid(getuid());
-      if (S_ISDIR(buf.st_mode)) {
-        d = opendir(max);
-        if (d) {
-          while ((dir = readdir(d))) {
-            if (dir -> d_name[0] != '.') {
-
-              days = strftime(time, 50, "%d", localtime( & buf.st_mtime));
-              if (days < 180) {
-
-                strftime(time, 50, "%B %d %H:%M", localtime( & buf.st_mtime));
-              } else {
-                strftime(time, 50, "%B %d %y %H:%M", localtime( & buf.st_mtime));
-              }
-
-             if(S_ISLNK(buf.st_mode)){
-                     printf((S_ISLNK(buf.st_mode)) ? "d" : "-");}
-                     else{
-              printf((S_ISDIR(buf.st_mode)) ? "d" : "-");}
-              printf((buf.st_mode & S_IRUSR) ? "r" : "-");
-              printf((buf.st_mode & S_IWUSR) ? "w" : "-");
-              printf((buf.st_mode & S_IXUSR) ? "x" : "-");
-              printf((buf.st_mode & S_IRGRP) ? "r" : "-");
-              printf((buf.st_mode & S_IWGRP) ? "w" : "-");
-              printf((buf.st_mode & S_IXGRP) ? "x" : "-");
-              printf((buf.st_mode & S_IROTH) ? "r" : "-");
-              printf((buf.st_mode & S_IWOTH) ? "w" : "-");
-              printf((buf.st_mode & S_IXOTH) ? "x" : "-");
-              printf(" ");
-              printf("%s", user -> pw_name);
-              printf(" ");
-              printf("%s", grp -> gr_name);
-              printf(" ");
-
-              if (sizeof(dir -> d_name) > 1000 && sizeof(dir -> d_name) < 1000000) {
-                bytesconvo = sizeof(dir -> d_name) / 1000;
-                printf("%ld", bytesconvo);
-                printf("%s", "K");
-              } else if (sizeof(dir -> d_name) > 1000000 && sizeof(dir -> d_name) < 1000000000) {
-                bytesconvo = sizeof(dir -> d_name) / 1000000;
-                printf("%ld", bytesconvo);
-                printf("%s", "M");
-              } else if (sizeof(dir -> d_name) > 1000000000) {
-                bytesconvo = sizeof(dir -> d_name) / 1000000000;
-                printf("%ld", bytesconvo);
-                printf("%s", "G");
-              } else {
-                printf("%ld", sizeof(dir -> d_name));
-              }
-
-              printf(" ");
-              printf("%s", time);
-              printf(" ");
-              printf(dir -> d_name);
-              printf("\n");
-            }
-          }
-          closedir(d);
-        }
-      }
-      return 0;
-    } else if (strchr(argv[i], '-') != NULL) {
+    }
+    /*if argument contains - but isnt an option will print not an option and list directory if final argument is a directory*/
+    else if (strchr(argv[i], '-') != NULL) {
 
       perror("not an option");
       if (S_ISDIR(buf.st_mode)) {
@@ -297,16 +266,10 @@ int main(int argc, char ** argv) {
       }
       return 0;
     } else {
-      perror("unsupported option doggo");
+      /*if all else fails then the option isnt supported and the program will terminate*/
+      perror("unsupported option");
     }
   }
-
-  if (argc == 1) {
-    perror("no arguments found");
-  }
-
   return 0;
 
-};
-
-
+}
